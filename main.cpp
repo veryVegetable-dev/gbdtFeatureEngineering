9652b0cda6d0a092e4ecc52c3ab32489df35769f
nomad <mengling_wu@icloud.com>
2021-05-25 21:20:13 +0800

    fix label pos

--- main.cpp
+++ main.cpp
@@ -41,9 +41,9 @@ namespace Eval {
 
 int main() {
     const int NUM_CLASS = 7;
-    const std::string& model_path = "model_path";
-    const std::string& data_path = "data_path";
-    const std::string& transformed_feature_path = "transformed_feature_path";
+    const std::string& model_path = "/Users/menglingwu/Documents/2021/coding/personal_projects/boosting_spark/xgboost.trees.parsed";
+    const std::string& data_path = "/Users/menglingwu/Documents/2021/coding/personal_projects/dataset/DryBeanDataset/Dry_Bean_Dataset.txt";
+    const std::string& transformed_feature_path = "/Users/menglingwu/Documents/2021/coding/personal_projects/dataset/DryBeanDataset/transformed_feature.txt";
 
     // 加载模型
     BoostingModel bm = BoostingModel::Get()->Init(model_path, NUM_CLASS);
@@ -74,17 +74,20 @@ int main() {
         const std::string &origin_label = labels_array[sample.label];
         size_t label_len = origin_label.size();
         size_t num_feature = transformed_features.size();
-        size_t buffer_len = label_len + num_feature + 2;
+        size_t buffer_len = label_len + 1 + num_feature * 2;
         char feature_buffer[buffer_len];
-        for (size_t i = 0; i < label_len; i++)
-            feature_buffer[i] = origin_label[i];
-        feature_buffer[label_len] = '|';
         for (size_t i = 0; i < num_feature; i++) {
-            if (transformed_features[i] == 1) feature_buffer[i+label_len+1] = '1';
-            else feature_buffer[i+label_len+1] = '0';
+            if (transformed_features[i] == 1) {
+                feature_buffer[2*i] = '1';
+            }
+            else
+                feature_buffer[2*i] = '0';
+            feature_buffer[2*i+1] = '|';
         }
-        feature_buffer[buffer_len] = '\n';
-        transformed_feature_file.write(feature_buffer, buffer_len+1);
+        for (size_t i = 0; i < label_len; i++)
+            feature_buffer[num_feature * 2 + i] = origin_label[i];
+        feature_buffer[buffer_len-1] = '\n';
+        transformed_feature_file.write(feature_buffer, buffer_len);
     }
     std::cout << error_cnt << "," << cnt << "," << (float)error_cnt / (float)cnt << "," << bm.getLeafNum() << std::endl;
     eval_file.close();
